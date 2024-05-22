adce32f94dc1bcae7a36c159a868ef8d63369698
digital-sparks <122462234+digital-sparks@users.noreply.github.com>
2024-05-22 19:02:53 +0200

    Disable email regex

--- src/form.js
+++ src/form.js
@@ -171,7 +171,7 @@ window.Webflow.push(() => {
       rules: {
         email: {
           email: true,
-          regex: /^\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i,
+          // regex: /^\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i,
         },
       },
       messages: {
